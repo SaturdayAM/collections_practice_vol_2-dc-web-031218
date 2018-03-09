30e64cab83101e2ebd67c3b50855f606620ca1ac
Jerry Lee <jwl62@georgetown.edu>
2018-03-09 19:49:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -81,3 +81,30 @@ def merge_data(keys, data)
     end
   end
 end
+
+
+keys = [
+      {
+          :first_name => "blake"
+      },
+      {
+          :first_name => "ashley"
+      }
+]
+
+data = [
+  {
+    "blake" => {
+       :awesomeness => 10,
+            :height => "74",
+         :last_name => "johnson"
+   },
+   "ashley" => {
+       :awesomeness => 9,
+            :height => 60,
+         :last_name => "dubs"
+   }
+ }
+]
+
+merge_data(keys, data)
